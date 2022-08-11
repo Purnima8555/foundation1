a6d77c5cf43e48a413745c2df76864b781d65304
Purnima Rana <punamranamagar637@gmail.com>
2022-08-11 11:56:53 +0545

    string in python

---
+++ new.py
@@ -0,0 +1,11 @@
+# --> It represents a one line comment in python
+
+...
+It represent a multi-line comment 
+We can write any thing here 
+...
+a=5 #here, a is a variablenwhich stores/holds integer type value i.e 5 
+naam = "Purnima" #here, naam is a variable which stores/holds string type value i.e "Purnima" 
+
+# And in pyhton string are always inside single or double quotes:
+#For example: "purnima" or "purnima"
